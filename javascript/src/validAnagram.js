124f0f9a6aef7cf42a6f5a808b8e36917372aebe
Dax Wann <daxwann@gmail.com>
2024-01-28 13:09:24 -0800

    add anagram answer

--- javascript/src/validAnagram.js
+++ javascript/src/validAnagram.js
@@ -4,8 +4,30 @@ letters of a different word or phrase, typically using
 all the original letters exactly once.
 */
 
-function validAnagram() {
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+function validAnagram(s1, s2) {
+  const charCount = {};
+  for (const c of s1) {
+    if (charCount[c]) {
+      charCount[c] = charCount[c] + 1;
+    } else {
+      charCount[c] = 1;
+    }
+  }
 
+  for (const c of s2) {
+    if (charCount[c] > 0) {
+      charCount[c] = charCount[c] - 1;
+    } else if (!charCount[c] || charCount[c] === 0) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 module.exports = { validAnagram }
